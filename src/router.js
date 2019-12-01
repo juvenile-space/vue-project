b35aaf45ddb178f122f6cfc2791e51e5bd39f7f0
juvenile-space <814301447@qq.com>
2019-12-01 17:41:57 +0800

    修改过后的

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import VueRouter from 'vue-router'
-
+import { removeDotSegments } from '_uri-js@4.2.2@uri-js'
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
     
-  ]
+  ],
+  linkActiveClass: 'mui-active'
 })
 
 // 把路由对象暴露出去
