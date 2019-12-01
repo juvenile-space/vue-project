9a0585d4ea241531d56da28d08790efa5c327bed
juvenile-space <814301447@qq.com>
2019-12-01 15:14:23 +0800

    my project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="index" >
+    <h1>这是 App 组件</h1>
+     <mt-header fixed title="fixed top"></mt-header>
+    <!-- <mybtn type="primary">12345</mybtn> -->
+   <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+  </div>
+</template>
+
+<script>
+// 按需导入 Toast 组件
+import { Toast } from "mint-ui";
+
+export default {
+}
+</script>
+
+
+<style lang="scss" scoped>
+.index {
+  padding-top: 40px;
+}
+</style>>
+
+
---
+++ src/test.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+        <h1>这是我们的App.vue组件</h1>
+        <mt-button type="default">default</mt-button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
